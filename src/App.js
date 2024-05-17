d3bfaa92c44f1dc84023f41e8491ba47791bc090
Serkan AYDIN <ssaydin@gmail.com>
2024-05-17 20:11:12 +0300

    request interceptor was added to appjs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./router";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { userInfo } = useSelector((state) => state.user);
+
+  axios.interceptors.request.use(
+    function (config) {
+      config.headers.Authorization = userInfo?.token;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
   return <RouterProvider router={router} />;
 }
 
